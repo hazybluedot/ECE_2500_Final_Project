2a0f7b77c462034dad6c74bd7f9265a65b488470
Darren Maczka <dmaczka@vt.edu>
2014-05-23 17:50:34 -0400

    refactor for table lookup

--- main.c
+++ main.c
@@ -3,13 +3,49 @@
 #include <string.h>
 #include <stdlib.h>
 
-const char* getColor1and2(int);
-const char* getColor3(long);
+typedef struct {
+    const char* color;
+    int sig;
+    double multiplier;
+} colorcode_t;
+
+static colorcode_t color_table[] = {
+    { "black",  0,  1   },
+    { "brown",  1,  1e1 },
+    { "red",    2,  1e2 },
+    { "orange", 3,  1e3 },
+    { "yellow", 4,  1e4 },
+    { "green",  5,  1e5 },
+    { "blue",   6,  1e6 },
+    { "violet", 7,  1e7 },
+    { "grey",   8,  1e8 },
+    { "white",  9,  1e9 },
+};
+
+#define COLOR_TABLE_LEN  sizeof(color_table)/sizeof(color_table[0])
+
 int  valueToColor(const char*);
 
 void usage(FILE* fp, int status, const char* progname) {
     fprintf(fp, "Usage: %s value\n", progname);
     exit(status);
+}
+
+const char* getColor(int i) {
+    if ( i >= COLOR_TABLE_LEN )
+        return "\0";
+    return color_table[i].color;
+}
+
+void frexp10(double x, int *digits, int *exp) {
+    char s[10];
+    
+    snprintf(s, sizeof(s), "%.2E", x);
+    digits[0] = s[0] - '0';
+    digits[1] = s[2] - '0';
+    digits[2] = s[3] - '0';
+    
+    *exp = strtol(s+6, NULL, 10);
 }
 
 int main(int argc, char *argv[]) {
@@ -38,66 +74,17 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-
 int valueToColor(const char* str) {	
-    char* end;
-    long val = strtol(str, &end, 10);
-    int digit1 = str[0] - '0';
-    int digit2 = str[1] - '0';
-    if ( *end != '\0') {
-        return -1;
-    }
+    char* endptr;
+    int digits[3];
+    int exp;
+    double value;
 
-    if (val < 10 || val > 9900000000) {
-        return -1;
-    }
-
-    printf("%s %s %s\n", getColor1and2(digit1), getColor1and2(digit2), getColor3(val));
+    value = strtod(str, &endptr); /* convert string to double */
+    
+    frexp10(value, digits, &exp); /* extract digits of significand and exponent */
+  
+    printf("%s %s %s\n", getColor(digits[0]), getColor(digits[1]), getColor(exp-1)); /* convert each digit to a color value and print the result */
     return 0;
 }
 
-const char* getColor1and2(int i) {
-    if (i == 0)
-        return "black";
-    else if (i == 1)
-        return "brown";
-    else if (i == 2)
-        return "red";
-    else if (i == 3)
-        return "orange";
-    else if (i == 4)
-        return "yellow";
-    else if (i == 5)
-        return "green";
-    else if (i == 6)
-        return "blue";
-    else if (i == 7)
-        return "violet";
-    else if (i == 8)
-        return "grey";
-    else if (i == 9)
-        return "white";
-    else 
-        return "xxx";
-}
-
-const char* getColor3(long i) {
-    if (i / 100000000 > 0)
-        return "violet";
-    else if (i / 10000000 > 0)
-        return "blue";
-    else if (i / 1000000 > 0)
-        return "green";
-    else if (i / 100000 > 0)
-        return "yellow";
-    else if (i / 10000 > 0)
-        return "orange";
-    else if (i / 1000 > 0)
-        return "red";
-    else if (i / 100 > 0)
-        return "brown";
-    else if (i / 10 > 0)
-        return "black";
-    else
-        return "xxx";
-}
